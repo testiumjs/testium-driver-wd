bbe2df991bff359534adce0b5e2018c94f8a7a97
Jan Krems <jan.krems@groupon.com>
2015-10-23 16:41:45 -0700

    Enable proxy test

--- test/integration/proxy.test.js
+++ test/integration/proxy.test.js
@@ -1,46 +1,37 @@
-import {getBrowser} from '../mini-testium-mocha';
+import {browser} from '../mini-testium-mocha';
+import {delay} from 'Bluebird';
 
-xdescribe('proxy', () => {
-  let browser;
-  before(async () => (browser = await getBrowser()));
+describe('proxy', () => {
+  before(browser.beforeHook);
 
   describe('handles errors', () => {
-    it('with no content type and preserves status code', () => {
-      browser.navigateTo('/');
-      browser.assert.httpStatus(200);
+    it('with no content type and preserves status code', () =>
+      browser
+        .navigateTo('/').assertStatusCode(200)
+        .navigateTo('/error').assertStatusCode(500));
 
-      browser.navigateTo('/error');
-      browser.assert.httpStatus(500);
-    });
-
-    it('that crash and preserves status code', () => {
-      browser.navigateTo('/crash');
-      browser.assert.httpStatus(500);
-    });
+    it('that crash and preserves status code', () =>
+      browser.navigateTo('/crash').assertStatusCode(500));
   });
 
-  it('handles request abortion', done => {
+  it('handles request abortion', async () => {
     // loads a page that has a resource that will
     // be black holed
-    browser.navigateTo('/blackholed-resource.html');
-    browser.assert.httpStatus(200);
+    await browser
+      .navigateTo('/blackholed-resource.html').assertStatusCode(200);
 
     // this can't simply be sync
     // because firefox blocks dom-ready
     // if we don't wait on the client-side
-    setTimeout(() => {
-      // when navigating away, the proxy should
-      // abort the resource request;
-      // this should not interfere with the new page load
-      // or status code retrieval
-      browser.navigateTo('/');
-      browser.assert.httpStatus(200);
-      done();
-    }, 50);
+    await delay(50);
+
+    // when navigating away, the proxy should
+    // abort the resource request;
+    // this should not interfere with the new page load
+    // or status code retrieval
+    await browser.navigateTo('/').assertStatusCode(200);
   });
 
-  it('handles hashes in urls', () => {
-    browser.navigateTo('/#deals');
-    browser.assert.httpStatus(200);
-  });
+  it('handles hashes in urls', () =>
+    browser.navigateTo('/#deals').assertStatusCode(200));
 });
