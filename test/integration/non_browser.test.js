89b1e75e8a5995b4b1a23fc5331716fef058414e
Jan Krems <jan.krems@groupon.com>
2015-10-23 16:16:36 -0700

    Add support for non-browser tests

--- test/integration/non_browser.test.js
+++ test/integration/non_browser.test.js
@@ -1,11 +1,10 @@
 import {get} from 'http';
 
-import {getBrowser} from '../mini-testium-mocha';
+import {browser} from '../mini-testium-mocha';
 import assert from 'assertive';
 
-xdescribe('Non-browser test', () => {
-  let browser;
-  before(async () => (browser = await getBrowser()));
+describe('Non-browser test', () => {
+  before(browser.beforeHook);
 
   it('can make a request without using the browser', done => {
     const url = `${browser.appUrl}/echo`;
