0e9338da48ccd4fcd304356a2638d45c2060447f
Jan Krems <jan.krems@groupon.com>
2015-10-24 12:44:34 -0700

    Port window tests

--- test/integration/window.test.js
+++ test/integration/window.test.js
@@ -1,46 +1,46 @@
-import {getBrowser} from '../mini-testium-mocha';
+import {browser} from '../mini-testium-mocha';
 import assert from 'assertive';
 
-xdescribe('window api', () => {
-  let browser;
-  before(async () => (browser = await getBrowser()));
+describe('window api', () => {
+  before(browser.beforeHook);
 
   describe('frames', () => {
-    before(() => {
-      browser.navigateTo('/windows.html');
-      browser.assert.httpStatus(200);
-    });
+    before(() =>
+      browser.navigateTo('/windows.html').assertStatusCode(200));
 
-    it('can be switched', () => {
-      browser.switchToFrame('cool-frame');
-      const iframeContent = browser.getElement('.in-iframe-only').get('text');
-      browser.switchToDefaultFrame();
-      const primaryContent = browser.getElement('.in-iframe-only');
+    it('can be switched', async () => {
+      await browser.switchToFrame('cool-frame');
+      const iframeContent = await browser.getElement('.in-iframe-only').text();
+      await browser.switchToDefaultFrame();
+      const primaryContent = await browser.getElementOrNull('.in-iframe-only');
       assert.equal('iframe content!', iframeContent);
       assert.equal(null, primaryContent);
     });
 
-    it('can be found when nested', () => {
-      browser.switchToFrame('cool-frame');
-      browser.switchToFrame('nested-frame');
-      const element = browser.getElement('#nested-frame-div');
-      assert.truthy('nested frame content', element);
+    it('can be found when nested', async () => {
+      await browser.switchToFrame('cool-frame');
+
+      await browser.assertElementExists('.in-iframe-only');
+      await browser.assertElementDoesntExist('#nested-frame-div');
+
+      await browser.switchToFrame('nested-frame');
+
+      await browser.assertElementDoesntExist('.in-iframe-only');
+      await browser.assertElementExists('#nested-frame-div');
     });
   });
 
   describe('popups', () => {
-    before(() => {
-      browser.navigateTo('/windows.html');
-      browser.assert.httpStatus(200);
-    });
+    before(() =>
+      browser.navigateTo('/windows.html').assertStatusCode(200));
 
-    it('can be opened', () => {
-      browser.clickOn('#open-popup');
-      browser.switchToWindow('popup1');
-      const popupContent = browser.getElement('.popup-only').get('text');
-      browser.closeWindow();
-      browser.switchToDefaultWindow();
-      const primaryContent = browser.getElement('.popup-only');
+    it('can be opened', async () => {
+      await browser.clickOn('#open-popup');
+      await browser.switchToWindow('popup1');
+      const popupContent = await browser.getElement('.popup-only').text();
+      await browser.close();
+      await browser.switchToDefaultWindow();
+      const primaryContent = await browser.getElementOrNull('.popup-only');
       assert.equal('popup content!', popupContent);
       assert.equal(null, primaryContent);
     });
