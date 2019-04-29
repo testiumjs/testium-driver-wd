0fa2987cc20315adff46a2d507d815eb9d5f564c
Junmin Liu <junminliu@gmail.com>
2019-04-29 15:34:52 -0500

    feat: Add new evaluateAsync function

--- test/integration/evaluate.test.js
+++ test/integration/evaluate.test.js
@@ -3,6 +3,9 @@
 const browser = require('../mini-testium-mocha').browser;
 const assert = require('assertive');
 const co = require('co');
+const getConfig = require('testium-core').getConfig;
+
+const browserName = getConfig().get('browser');
 
 describe('evaluate', () => {
   before(browser.beforeHook());
@@ -38,6 +41,102 @@ describe('evaluate', () => {
         yield browser.evaluate(3, 6, function(a, b) {
           return a * b;
         })
+      );
+    })
+  );
+});
+
+describe('evaluateAsync', () => {
+  if (browserName === 'phantomjs') {
+    xit(
+      "skipping tests because browser phantomjs doesn't support evaluateAsync"
+    );
+    return;
+  }
+
+  before(browser.beforeHook());
+
+  before(() => browser.loadPage('/'));
+
+  it(
+    'runs JavaScript passed as a Function',
+    co.wrap(function*() {
+      assert.equal(6, yield browser.evaluateAsync(() => 6));
+    })
+  );
+
+  it(
+    'runs JavaScript passed as a Function with optional prepended args',
+    co.wrap(function*() {
+      assert.equal(18, yield browser.evaluateAsync(3, 6, (a, b) => a * b));
+    })
+  );
+
+  it(
+    'runs JavaScript passed as an Async Function with promise',
+    co.wrap(function*() {
+      assert.equal(
+        18,
+        yield browser.evaluateAsync(
+          3,
+          6,
+          (a, b) =>
+            new Promise(resolve => {
+              setTimeout(() => {
+                resolve(a * b);
+              }, 1000);
+            })
+        )
+      );
+    })
+  );
+
+  it(
+    'runs JavaScript passed as an Async Function with promise and no param',
+    co.wrap(function*() {
+      assert.equal(
+        'foo',
+        yield browser.evaluateAsync(() => {
+          return Promise.resolve('foo');
+        })
+      );
+    })
+  );
+
+  it(
+    'runs JavaScript passed as an Async Function returns array',
+    co.wrap(function*() {
+      assert.deepEqual(
+        [['foo']],
+        yield browser.evaluateAsync(() => {
+          return Promise.resolve([['foo']]);
+        })
+      );
+    })
+  );
+
+  it(
+    'runs JavaScript passed as an Async Function with promise error',
+    co.wrap(function*() {
+      assert.equal(
+        'promise error',
+        yield browser
+          .evaluateAsync(() => Promise.reject(new Error('promise error')))
+          .catch(error => error.message)
+      );
+    })
+  );
+
+  it(
+    'runs JavaScript passed as an Async Function with exception',
+    co.wrap(function*() {
+      assert.equal(
+        'async error',
+        yield browser
+          .evaluateAsync(() => {
+            throw new Error('async error');
+          })
+          .catch(error => error.message)
       );
     })
   );
