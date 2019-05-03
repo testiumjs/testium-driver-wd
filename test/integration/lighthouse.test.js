64dc4188e3cf6ed61adf79ce8f5b9868a9789b38
Junmin Liu <junminliu@gmail.com>
2019-05-02 20:59:01 -0500

    feat: new a11yAudit function

---
+++ test/integration/lighthouse.test.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const assert = require('assertive');
+const getConfig = require('testium-core').getConfig;
+
+const browser = require('../mini-testium-mocha').browser;
+
+const browserName = getConfig().get('browser');
+
+describe('navigation', () => {
+  if (browserName !== 'chrome') {
+    xit('Skipping lighthouse tests. They only work for chrome.');
+    return;
+  }
+
+  before(browser.beforeHook());
+
+  describe('lighthouse', () => {
+    describe('getLighthouseData', () => {
+      it('can get lighthouse report data', async () => {
+        const results = await browser
+          .navigateTo('/draggable.html')
+          .assertStatusCode(200)
+          .getLighthouseData();
+        const report = results.report;
+        assert.match(/\/draggable\.html$/, JSON.parse(report).finalUrl);
+      });
+    });
+  });
+
+  describe('a11y', () => {
+    it('can audit the accessibility issues', async () => {
+      const results = await browser
+        .navigateTo('/draggable.html')
+        .assertStatusCode(200)
+        .a11yAudit();
+      assert.equal(2, results.length);
+      assert.equal('document-title', results[0].auditId);
+      assert.equal('html-has-lang', results[1].auditId);
+    });
+
+    it('can audit the accessibility issues with ignore array', async () => {
+      const results = await browser
+        .navigateTo('/draggable.html')
+        .assertStatusCode(200)
+        .a11yAudit({ ignore: ['document-title'] });
+      assert.equal(1, results.length);
+      assert.equal('html-has-lang', results[0].auditId);
+    });
+
+    it('can audit the accessibility issues with ignore function', async () => {
+      const results = await browser
+        .navigateTo('/draggable.html')
+        .assertStatusCode(200)
+        .a11yAudit({
+          ignore: violation =>
+            ['document-title', 'html-has-lang'].includes(violation.id),
+        });
+      assert.equal(0, results.length);
+    });
+  });
+});
--- test/integration/puppeteer.test.js
+++ test/integration/puppeteer.test.js
@@ -3,7 +3,6 @@
 const assert = require('assertive');
 const co = require('co');
 const getConfig = require('testium-core').getConfig;
-const Semver = require('semver');
 
 const browser = require('../mini-testium-mocha').browser;
 
@@ -16,30 +15,6 @@ describe('navigation', () => {
   }
 
   before(browser.beforeHook());
-
-  if (Semver.satisfies(process.version, '>=8.0.0')) {
-    it(
-      'can get lighthouse report data',
-      co.wrap(function*() {
-        yield browser.navigateTo('/draggable.html').assertStatusCode(200);
-        const results = yield browser.getLighthouseData();
-        const report = results.report;
-        assert.match(/\/draggable\.html$/, JSON.parse(report).finalUrl);
-      })
-    );
-  } else {
-    it(
-      'is helpful in the way it reports the lack of lighthouse support',
-      co.wrap(function*() {
-        yield browser.navigateTo('/draggable.html').assertStatusCode(200);
-        const error = yield assert.rejects(browser.getLighthouseData());
-        assert.equal(
-          'Lighthouse requires a newer version of node',
-          error.message
-        );
-      })
-    );
-  }
 
   it(
     'exposes device emulation',
