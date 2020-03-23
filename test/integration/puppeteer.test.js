be851738db28ef5baab2ef4668289309c51e844c
Andreas Richter <andreas.richter@groupon.com>
2020-03-23 11:59:00 +0100

    feat: upgrade to puppeteer 2.x
    BREAKING CHANGE:
    withPuppeteerPage() will have a puppeteer 2.x Page object as function argument

--- test/integration/puppeteer.test.js
+++ test/integration/puppeteer.test.js
@@ -3,11 +3,11 @@
 const assert = require('assertive');
 const getConfig = require('testium-core').getConfig;
 
-const browser = require('../mini-testium-mocha').browser;
+const { browser } = require('../mini-testium-mocha');
 
 const browserName = getConfig().get('browser');
 
-describe.skip('navigation', () => {
+describe('puppeteer', () => {
   if (browserName !== 'chrome') {
     it.skip('Skipping puppeteer tests. They only work for chrome.');
     return;
@@ -17,13 +17,26 @@ describe.skip('navigation', () => {
 
   it('exposes device emulation', async () => {
     await browser.emulate('iPhone 6');
-    await browser.navigateTo('/').assertStatusCode(200);
+    await browser.loadPage('/');
     assert.include(
-      'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X)',
+      'Mozilla/5.0 (iPhone; CPU iPhone OS',
       await browser.evaluate(() => {
         /* eslint-env browser */
         return navigator.userAgent;
       })
     );
+  });
+
+  describe('browser.withPuppeteerPage', () => {
+    it('exposes puppeteer Page in callback argument', async () => {
+      const page = await browser.withPuppeteerPage(Page => Page);
+
+      assert.hasType(Function, page.workers);
+      assert.hasType(Function, page.browserContext);
+    });
+
+    it('executes callback function', done => {
+      browser.withPuppeteerPage(() => done());
+    });
   });
 });
